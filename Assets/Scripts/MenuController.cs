8cd0482d0f370a1c963c88038b0ac52001a3d75a
Zael41 <zaelgaming@gmail.com>
2022-05-10 20:27:24 +0200

    volumeSlider

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MenuController : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
     public void StartGame()
     {
         SceneManager.LoadScene("ExteriorScene");
-    }
-
-    public void Settings()
-    {
-        Debug.Log("Settings touched");
     }
 
     public void QuitGame()
     {
         Debug.Log("Quitting game");
         Application.Quit();
+    }
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
     }
 }
